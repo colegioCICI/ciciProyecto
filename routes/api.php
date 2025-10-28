61c338f30f067ca393d5058d2b681cf078cab41e
Kevin Guacanes <imdresguacanes@gmail.com>
2025-10-28 13:06:13 -0500

    Correccion para descargas de documentos

--- routes/api.php
+++ routes/api.php
@@ -12,10 +12,9 @@ Route::get('/api/hello', function () {
     return response()->json(['message' => 'Hello World from CICI API']);
 });
 
-
+Route::get('/documents/{id}/download', [DocumentController::class, 'download']);
 Route::middleware(['api.token'])->group(function () {
     Route::get('/folders/{folder_id}', [FolderController::class, 'show']);
-    Route::get('/documents/{id}/download', [DocumentController::class, 'download']);
     Route::get('/documentos/tramiteCICI/{tramite}', [TramiteRequerimientosController::class, 'getDocumentosPorTramite']);
     Route::get('/emailsCICI/{tramite}', [TramiteRequerimientosController::class, 'getEmailsPorTramite']);
 });
